767a68f7b27cbc1bb54d0e87121440c6cf50c009
JBR_dev_10 <juneider.br.dev@outlook.com>
2026-09-18 19:19:52 -0500

    Refactor project structure

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from config import APP_VERSION
+
+import config
+
+app = FastAPI(tittle="student-api", version=config.APP_VERSION)
+
+@app.get("/health")
+def health():
+    return("status": "ok")
+
+@app.get("/students")
+def list_students():
+    return [{"id":1, "name": "Ana"}, {"id": 2, "name": "Luis"}]
